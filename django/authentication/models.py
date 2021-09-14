4150ffa676c34e1c13013cabccf3c8bc06a7ae34
MuriloScarpaSitonio <muriloscarpa@gmail.com>
2021-09-13 21:46:14 -0300

    add docker support

--- django/authentication/models.py
+++ django/authentication/models.py
@@ -9,8 +9,8 @@ class CustomUser(AbstractUser):
     cpf = models.CharField(max_length=14, validators=[validate_cpf])
     cei_password = models.CharField(max_length=128)
 
-    REQUIRED_FIELDS = ["cpf", "cei_password"]
+    # REQUIRED_FIELDS = ["cpf", "cei_password"]
 
     def save(self, *args, **kwargs):
-        self.cei_password = make_password(self.cei_password)
+        # self.cei_password = make_password(self.cei_password)
         super().save(*args, **kwargs)
---
+++ django/entrypoint.sh
@@ -0,0 +1,11 @@
+python manage.py migrate --noinput
+
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$ADMIN_USERNAME').exists():
+    User.objects.create_superuser(username='$ADMIN_USERNAME', password='$ADMIN_PASSWORD')
+    print("Superuser created!")
+EOF
+
+exec "$@"
