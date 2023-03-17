cda19ad48f983df53e7f2199e673aa47b41d0f7d
MuriloScarpaSitonio <muriloscarpa@gmail.com>
2023-03-17 08:35:15 -0300

    add locust

---
+++ django/locustfile.py
@@ -0,0 +1,23 @@
+from locust import events, HttpUser, task
+
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument("--username", type=str, env_var="LOCUST_USERNAME")
+    parser.add_argument("--password", type=str, env_var="LOCUST_PASSWORD")
+
+
+class QuickstartUser(HttpUser):
+    @task
+    def full_assets_list(self):
+        self.client.get("/assets?page_size=100")
+
+    def on_start(self):
+        r = self.client.post(
+            "/token",
+            json={
+                "username": self.environment.parsed_options.username,
+                "password": self.environment.parsed_options.password,
+            },
+        )
+        self.client.headers = {"Authorization": f"Bearer {r.json()['access']}"}
